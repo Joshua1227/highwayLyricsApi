055c597dd7bc55cb7758d944f92340d0e964ea49
Sandeep Daniel <sandeepjoshuadaniel@gmail.com>
2025-03-08 15:39:33 +0530

    added id to songs

--- models/song.go
+++ models/song.go
@@ -3,10 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Song struct {
-	Title      string `bson:"title" json:"title"`
-	Lyrics     string `bson:"lyrics" json:"lyrics"`
-	AddedBy    string `bson:"addedby" json:"added_by"`
-	ApprovedBy string `bson:"approvedby" json:"approved_by"`
+	Id         primitive.ObjectID `bson:"_id" json:"id"`
+	Title      string             `bson:"title" json:"title"`
+	Lyrics     string             `bson:"lyrics" json:"lyrics"`
+	AddedBy    string             `bson:"addedby" json:"added_by"`
+	ApprovedBy string             `bson:"approvedby" json:"approved_by"`
 	// language
 	// writer
 	// tags
